82ad127b5789ba2e076e6b7a07b65443c0293664
Jashany <jsingh12_be22@thapar.edu>
2024-06-16 08:21:08 +0530

    backend

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ import ConnectDB from './db/index.js';
 
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import MitraRouter from './routes/GoatMitra.js';
+import GoatRouter from './routes/Goats.js';
+import PalakRouter from './routes/GoatPalak.js';
 
 const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/mitra', MitraRouter);
+app.use('/goat', GoatRouter);
+app.use('/palak', PalakRouter);
 
 ConnectDB().then(() => {
     app.listen(5000, () => {
